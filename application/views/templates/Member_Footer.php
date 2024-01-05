801e65a45425510950f51f609476f7c2ffdfc40c
heeren17 <miyukichan003@gmail.com>
2024-01-06 04:42:36 +0700

    sistem_pakar_KP

---
+++ application/views/templates/Member_Footer.php
@@ -0,0 +1,15 @@
+
+</div>
+
+
+
+</body>
+
+<script src="<?= base_url('assets2'); ?>/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/bootstrap.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/awesome-landing-page.js" type="text/javascript"></script>
+<script src="<?= base_url('assets2'); ?>/js/script.js" type="text/javascript">
+</script>
+
+</html>
